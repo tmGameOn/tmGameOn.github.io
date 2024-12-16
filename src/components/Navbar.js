1fde61c1445d3a997b954a39ca648439775961fe
tmGameOn <mocxsam@gmail.com>
2024-12-15 23:32:27 -0300

    add responsiv

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Navbar.css";
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="navbar">
             <h1>T&M GameOn Imports</h1>
-            <ul>
+            {/* Ícone do menu hambúrguer */}
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+
+            {/* Links do Navbar */}
+            <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
                 <li><a href="#home">Home</a></li>
+                <li>
+                    <a
+                        href="https://wa.me/5511999999999" // Número do WhatsApp (substitua pelo seu)
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Contato
+                    </a>
+                </li>
                 <li><a href="#about">Sobre Nós</a></li>
-                <li><a href="#contact">Contato</a></li>
             </ul>
         </nav>
     );
